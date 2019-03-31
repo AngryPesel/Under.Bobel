b82a800dfb635f02d615944a11a8d8e6d5b55b71
Aleksandr <hnatenko.deltaworld@gmail.com>
2019-03-31 14:54:00 +0300

    Add GG custom

---
+++ Platformer 2.0/Assets/Custom.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Custom : MonoBehaviour
+{
+    public Sprite LGBT_GG;
+
+    public void LGBT()
+    {
+        gameObject.GetComponent<SpriteRenderer>().sprite = LGBT_GG;
+        
+    }
+
+
+}
